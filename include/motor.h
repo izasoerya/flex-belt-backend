0c5522cdaa2e8057216dde7e9e67cbe07fed3e0b
izasoerya <122797748+izasoerya@users.noreply.github.com>
2025-04-30 18:10:19 +0700

    Update motor.h

--- include/motor.h
+++ include/motor.h
@@ -1,69 +1,55 @@
 #pragma once
 
 #include <Arduino.h>
-#include "BTS7960.h"
+
+#define LPWM 18
+#define RPWM 19
+#define REN 5
+#define LEN 17
 
 #define MAX_SPEED 255
 
 class Motor
 {
 private:
-    const uint8_t LPWM = 18;
-    const uint8_t RPWM = 19;
-    const uint8_t REN = 5;
-    const uint8_t LEN = 17;
-
-    BTS7960 *motorController = nullptr;
-
     bool isCold = true;
     byte heater = 0;
 
 public:
-    Motor() {}
+    Motor() {};
 
     void begin()
     {
-        motorController = new BTS7960(LEN, REN, LPWM, RPWM);
-        motorController->Enable();
-    }
+        pinMode(LPWM, OUTPUT);
+        pinMode(RPWM, OUTPUT);
+        pinMode(REN, OUTPUT);
+        pinMode(LEN, OUTPUT);
+
+        digitalWrite(REN, HIGH);
+        digitalWrite(LEN, HIGH);
+    };
 
     void setConfig(bool isCold, byte heater)
     {
         this->isCold = isCold;
         this->heater = heater;
-    }
+    };
 
     void setSpeed()
     {
-        if (!motorController)
-            return;
-
-        uint16_t heaterSpeed = map(heater, 0, 100, 0, MAX_SPEED);
-
         if (isCold)
         {
-            motorController->TurnRight(heaterSpeed);
+            uint16_t heaterSpeed = map(heater, 0, 100, 0, MAX_SPEED);
+            digitalWrite(LPWM,LOW);            
+            analogWrite(LPWM, 0);
+            analogWrite(RPWM, heaterSpeed);
         }
-        else
+        if (!isCold)
         {
-            motorController->TurnLeft(heaterSpeed);
-        }
-
-        delayMicroseconds(100);
-    }
-
-    void stop()
-    {
-        if (motorController)
-        {
-            motorController->Stop();
-            motorController->Disable();
-        }
-    }
-
-    ~Motor()
-    {
-        stop();
-        delete motorController;
-    }
+            uint16_t heaterSpeed = map(heater, 0, 100, 0, MAX_SPEED);
+            digitalWrite(RPWM,LOW);
+            analogWrite(RPWM, 0);
+            analogWrite(LPWM, heaterSpeed);
+        }
+    }
 };
